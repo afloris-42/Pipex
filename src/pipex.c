a084792926ccd38bff43db2fe53af5f6e1809619
Aurelio Floris <babyf@MacBook-Air-di-Auri.local>
2025-05-04 15:22:27 +0200

    without printf and libft

---
+++ src/pipex.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 15:55:25 by babyf             #+#    #+#             */
+/*   Updated: 2025/05/03 16:18:25 by babyf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	check_files(char **)
+{
+
+}
+
+//envp is an array of pointers to environment variables
+int main (int argc, char **argv, char **envp)
+{
+	//variable declarations
+	int	fd[2];
+	int	infile;
+	int outfile;
+	//check argument count
+
+	//
+}
